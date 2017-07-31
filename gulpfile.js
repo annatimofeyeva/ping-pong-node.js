479fcaacd5d74808df16c477c4cc1f602e5fdaf6
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-07-30 17:34:45 -0700

    add minification with gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('myTask', function(){
   console.log('hello gulp');
@@ -18,6 +19,12 @@ gulp.task('concatInterface', function() {
   return gulp.src(['./js/pingpong-interface.js', './js/signup-interface.js'])
     .pipe(concat('allConcat.js'))
     .pipe(gulp.dest('./tmp'));
+});
+//remove all white spaces in App.js
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
 });
 
 //more dependencies will be added here.
