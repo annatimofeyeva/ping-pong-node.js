020ac1ec5f2129a3e3596488b12a1d63a0b97d48
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-07-30 17:19:46 -0700

    concatination client-side js files before browserifying - email input field added

--- build/js/app.js
+++ build/js/app.js
@@ -1,20 +1,4 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var Calculator = require ('./../js/pingpong.js').calculatorModule;
-
-$(document).ready(function() {
-  $('#ping-pong-form').submit(function(event) {
-    event.preventDefault();
-    var goal = $('#goal').val();
-    //create a Calculator object and then we call pingPong() on it
-    var simpleCalculator = new Calculator("hot pink");
-    var output = simpleCalculator.pingPong(goal);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
-    });
-  });
-});
-
-},{"./../js/pingpong.js":2}],2:[function(require,module,exports){
 //creating a Calculator with "pretty looking skins" - and ability to perform the ping-pong game
 //
 
@@ -77,4 +61,29 @@ $(document).ready(function() {
   });
 });*/
 
-},{}]},{},[1]);
+},{}],2:[function(require,module,exports){
+var Calculator = require ('./../js/pingpong.js').calculatorModule;
+
+$(document).ready(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+    var goal = $('#goal').val();
+    //create a Calculator object and then we call pingPong() on it
+    var simpleCalculator = new Calculator("hot pink");
+    var output = simpleCalculator.pingPong(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
+
+$(document).ready(function(){
+  $('#signup').submit(function(event){
+    event.preventDefault();
+    var email = $('#email').val();
+    $('#signup').hide();
+    $('#solution').prepend('<p>Thank you, ' + email + ' has been added to our list!</p>');
+  });
+});
+
+},{"./../js/pingpong.js":1}]},{},[2]);
