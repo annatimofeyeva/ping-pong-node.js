e1265280cc1ac54deae301d1f5ba859e53b6811f
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-08-01 14:44:49 -0700

    add openweathermap.org api to the project

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -32,10 +32,8 @@ $(document).ready(function() {
   $('#weather-location').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $('.showWeather').text("The city you have chosen is " + city + ".");
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      console.log(response);
-      console.log(JSON.stringify(response));
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
     });
   });
 });
