16f690d809fc3ad799f8df8089689e31acb09b9b
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-08-01 11:58:24 -0700

    add BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,8 @@ var lib = require('bower-files')({
     }
   }
 });
+var browserSync = require('browser-sync').create();
+
 
 
 var buildProduction = utilities.env.production;
@@ -79,6 +81,26 @@ gulp.task('build', ['clean'], function(){
     gulp.start('jsBrowserify');
   }
   gulp.start('bower');
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+});
+
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browserSync.reload();
+});
+
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
 });
 
 //to fix syntax issues - run periodically with //$gulp jshint to fix bugs
