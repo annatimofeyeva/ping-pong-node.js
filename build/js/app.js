ac22c5cc5032b9c851a4f0f9a8b6c8de648d1cf8
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-08-01 16:20:23 -0700

    refactoring code with promice method then()

--- build/js/app.js
+++ build/js/app.js
@@ -96,10 +96,29 @@ $(document).ready(function() {
   $('#weather-location').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    });
-  });
+
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+     .then(function(response) {
+         $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+      });
+   });
 });
+
+//refuctoring code
+
+/*
+$(document).ready(function() {
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    //$.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      //$('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+
+      $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+        console.log("The humidity in " + city + " is " + response.main.humidity + "%");
+    });
+    console.log("Notice: The GET request has been made.");
+  });
+}); */
 
 },{"./../js/pingpong.js":1}]},{},[2]);
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -32,8 +32,27 @@ $(document).ready(function() {
   $('#weather-location').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    });
-  });
+
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+     .then(function(response) {
+         $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+      });
+   });
 });
+
+//refuctoring code
+
+/*
+$(document).ready(function() {
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    //$.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      //$('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+
+      $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+        console.log("The humidity in " + city + " is " + response.main.humidity + "%");
+    });
+    console.log("Notice: The GET request has been made.");
+  });
+}); */
